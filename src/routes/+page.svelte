a03e397d12f7408c164e91cf36c013a6d37ea3ef
Diego-Della-Vecchia <155764085+Diego-Della-Vecchia@users.noreply.github.com>
2024-11-19 20:58:07 +0000

    Adjust logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,72 +3,32 @@
 
 	let showing = $state(false);
 
-	let rounds: { title: string; body: { img: string; text: string } }[][] = [
-		[
-			{
-				title: 'Test',
-				body: {
-					img: 'https://www.wwf-junior.de/fileadmin/user_upload/Tiere/1440_Fledermaeuse_Grosser_Abendsegler_Zaehne__c__imago_images.jpg',
-					text: 'das ist ein Test'
-				}
-			},
-			{
-				title: 'Test',
-				body: {
-					img: 'https://www.wwf-junior.de/fileadmin/user_upload/Tiere/1440_Fledermaeuse_Grosser_Abendsegler_Zaehne__c__imago_images.jpg',
-					text: 'das ist ein Test'
-				}
-			},
-			{
-				title: 'Test',
-				body: {
-					img: 'https://www.wwf-junior.de/fileadmin/user_upload/Tiere/1440_Fledermaeuse_Grosser_Abendsegler_Zaehne__c__imago_images.jpg',
-					text: 'das ist ein Test'
-				}
-			}
-		],
-		[
-			{
-				title: 'Test',
-				body: {
-					img: 'https://www.wwf-junior.de/fileadmin/user_upload/Tiere/1440_Fledermaeuse_Grosser_Abendsegler_Zaehne__c__imago_images.jpg',
-					text: 'das ist ein Test'
-				}
-			},
-			{
-				title: 'Test',
-				body: {
-					img: 'https://www.wwf-junior.de/fileadmin/user_upload/Tiere/1440_Fledermaeuse_Grosser_Abendsegler_Zaehne__c__imago_images.jpg',
-					text: 'das ist ein Test'
-				}
-			},
-			{
-				title: 'Test',
-				body: {
-					img: 'https://www.wwf-junior.de/fileadmin/user_upload/Tiere/1440_Fledermaeuse_Grosser_Abendsegler_Zaehne__c__imago_images.jpg',
-					text: 'das ist ein Test'
-				}
-			}
-		]
+	let rounds: { title: string; img: string; options: string[] }[] = [
+		{
+			title: 'Wie gross sind Fledermäuse',
+			img: 'https://www.fledermaus-bayern.de/files/upload/Biologie%20und%20Allgemeines/K%C3%B6perbau-Schema.jpg',
+			options: ['1-5cm', '5-10cm', '10-15cm']
+		}
 	];
 
 	let solutions: { text: string; index: number }[] = [
-		{ text: 'String', index: 0 },
-		{ text: 'String', index: 0 }
+		{
+			text: 'Die in Deutschland lebenden Fledermäuse sind eher klein. Ihr Körper ist selten länger als 5 Zentimeter.',
+			index: 0
+		}
 	];
 
 	let score = $state(0);
-
-
 </script>
 
 <h1>Rette die Fledermäuse</h1>
 <h2>Score: {score}</h2>
 
 {#if round < rounds.length}
-	<div class="container">
-		{#if !showing}
-			{#each rounds[round] as current, i (current)}
+	<img src={rounds[round].img} alt="" />
+	{#if !showing}
+		<dv class="container">
+			{#each rounds[round].options as option, i (option)}
 				<button
 					class="card"
 					onclick={() => {
@@ -77,30 +37,30 @@
 						} else {
 							score -= 50;
 						}
-
 						showing = true;
-
 						setTimeout(() => {
 							showing = false;
 							round++;
 						}, 4000);
 					}}
 				>
-					<h2>{current.title}</h2>
-					<img src={current.body.img} alt="" />
-					<p>{current.body.text}</p>
+					{option}
 				</button>
 			{/each}
-		{:else}
+		</dv>
+	{:else}
+		<div class="container">
 			<div class="card">
 				<h2>Lösung</h2>
 				<p>{solutions[round].text}</p>
 			</div>
-		{/if}
-	</div>
+		</div>
+	{/if}
 {:else if score > 300}
+	<img src="https://i.ytimg.com/vi/WdFa450Kb1w/maxresdefault.jpg" alt="" />
 	<h2>Du hast sie gerettet</h2>
 {:else}
+	<img src="https://i.ytimg.com/vi/WdFa450Kb1w/maxresdefault.jpg" alt="" />
 	<h2>Du hast sie nicht gerettet</h2>
 {/if}
 
@@ -121,6 +81,7 @@
 		justify-content: space-around;
 		flex-wrap: wrap;
 		gap: 20px;
+		padding-top: 50px;
 	}
 
 	.card {
@@ -129,12 +90,11 @@
 		border-radius: 10px;
 		width: 80vw;
 		max-width: 300px;
-		height: 150vw;
-		max-height: 400px;
 		animation: fly 0.2s ease-in forwards;
 		background-color: white;
 		transition: transform 0.2s ease-in-out;
 		display: block;
+		font-size: 30px;
 	}
 
 	.card:hover {
@@ -157,7 +117,7 @@
 	}
 
 	img {
-		width: 80%;
+		width: 200px;
 		margin: auto;
 		border-radius: 10px;
 		display: block;
