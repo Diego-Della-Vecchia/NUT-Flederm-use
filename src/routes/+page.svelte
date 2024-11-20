9c28cb6c4bc99295d54499f95e09c49c064280a4
Diego-Della-Vecchia <155764085+Diego-Della-Vecchia@users.noreply.github.com>
2024-11-20 08:24:08 +0000

    Final commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,23 +5,68 @@
 
 	let rounds: { title: string; img: string; options: string[] }[] = [
 		{
-			title: 'Wie gross sind Fledermäuse',
+			title: 'Wie groß sind Fledermäuse?',
 			img: 'https://www.fledermaus-bayern.de/files/upload/Biologie%20und%20Allgemeines/K%C3%B6perbau-Schema.jpg',
-			options: ['1-5cm', '5-10cm', '10-15cm']
+			options: ['1-5cm', '5-10cm', '10.23-15.67cm']
+		},
+		{
+			title: 'Wie viel wiegen Fledermäuse?',
+			img: 'https://www.fledermaus-bayern.de/files/upload/Biologie%20und%20Allgemeines/K%C3%B6perbau-Schema.jpg',
+			options: ['200-400g', '500-800g', '2-200g']
+		},
+		{
+			title: 'Wie alt werden Fledermäuse?',
+			img: 'https://www.nistkasten-online.de/blog/wp-content/uploads/2024/06/Fledermaus-gefunden.jpeg',
+			options: ['5-15 Jahre', '10-30 Jahre', '3-5 Jahre']
+		},
+		{
+			title: 'Was essen Fledermäuse?',
+			img: 'https://www.wwf-junior.de/fileadmin/user_upload/Tiere/1440_Fledermaeuse_Grosser_Abendsegler_Zaehne__c__imago_images.jpg',
+			options: ['Insekten', 'Beeren', 'Tierblut']
+		},
+		{
+			title: 'Wie gut können Fledermäuse sehen?',
+			img: 'https://149363556.v2.pressablecdn.com/wp-content/uploads/2016/08/fledermaus-2.jpg',
+			options: ['Sehr gut', 'Bischen', 'Gar nicht']
+		},
+		{
+			title: 'Zu welcher Tierklasse gehören Fledermäuse?',
+			img: 'https://d1g9li960vagp7.cloudfront.net/wp-content/uploads/2021/05/Wirbeltierklassen-Evolution-1024x576.jpg',
+			options: ['Reptilien', 'Vögel', 'Säugetiere']
 		}
 	];
 
-	let solutions: { text: string; index: number }[] = [
+	let solutions: { text: string; index: number[] }[] = [
 		{
-			text: 'Die in Deutschland lebenden Fledermäuse sind eher klein. Ihr Körper ist selten länger als 5 Zentimeter.',
-			index: 0
+			text: 'Ihr Körper ist selten länger als 5 Zentimeter.',
+			index: [0] // Corresponds to "1-5"
+		},
+		{
+			text: 'Eine Fledermaus wiegt etwa 2-200 Gramm.',
+			index: [2] // Corresponds to "2-200G"
+		},
+		{
+			text: 'Fledermäuse werden etwa 10-30 Jahre alt.',
+			index: [1] // Corresponds to "10-30"
+		},
+		{
+			text: 'Fledermäuse essen hauptsächlich Insekten oder sie ernähren sich Vegetarisch. Es gibt hingegen aber eine Art von Fledermäuse die Blut saugen daher stammt Dracula mytos von Dracula.',
+			index: [0, 1, 2] // Corresponds to "Insekten, Beeren"
+		},
+		{
+			text: 'Fledermäuse sind nicht blind, sondern können hell und dunkel unterscheiden.',
+			index: [1] // Corresponds to "Bischen"
+		},
+		{
+			text: 'Fledermäuse gehören tatsächlich zu der Tierklasse Säugetiere.',
+			index: [2] // Corresponds to "Säugetiere"
 		}
 	];
 
 	let score = $state(0);
 </script>
 
-<h1>Rette die Fledermäuse</h1>
+<h1>Fledermaus Quiz</h1>
 <h2>Score: {score}</h2>
 
 {#if round < rounds.length}
@@ -32,10 +77,12 @@
 				<button
 					class="card"
 					onclick={() => {
-						if (i == solutions[i].index) {
+						if (solutions[round].index.includes(i)) {
 							score += 100;
 						} else {
-							score -= 50;
+							if (score != 0) {
+								score -= 50;
+							}
 						}
 						showing = true;
 						setTimeout(() => {
@@ -58,10 +105,10 @@
 	{/if}
 {:else if score > 300}
 	<img src="https://i.ytimg.com/vi/WdFa450Kb1w/maxresdefault.jpg" alt="" />
-	<h2>Du hast sie gerettet</h2>
+	<h2>Du hast gewonnen</h2>
 {:else}
 	<img src="https://i.ytimg.com/vi/WdFa450Kb1w/maxresdefault.jpg" alt="" />
-	<h2>Du hast sie nicht gerettet</h2>
+	<h2>Du hast verloren</h2>
 {/if}
 
 <style>
